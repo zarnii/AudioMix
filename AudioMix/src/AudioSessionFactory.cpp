c9ba74cf256353bc06d0b6566aa1421f0b0cf734
zarnii <tryakin_gd1@mail.ru>
2024-08-07 21:07:25 +0300

    Отделена логика создания аудиосессий

---
+++ AudioMix/src/AudioSessionFactory.cpp
@@ -0,0 +1,97 @@
+﻿#include "AudioSessionFactory.h"
+
+namespace AudioMix
+{
+	AudioSessionFactory::AudioSessionFactory(std::shared_ptr<ILogger> logger)
+	{
+		if (logger == nullptr)
+		{
+			throw std::invalid_argument("Logger is null.");
+		}
+
+		_logger = logger;
+	}
+
+	void AudioSessionFactory::CheckHResultIsCorrect(HRESULT hResult, const std::string& errorMessage)
+	{
+		if (FAILED(hResult))
+		{
+			_logger->LogError(errorMessage,
+				std::string(NAMEOF(SliderMapper::CheckHResult)));
+			throw std::runtime_error(errorMessage);
+		}
+	}
+
+	std::vector<std::shared_ptr<AudioSession>> 
+		AudioSessionFactory::GetAudioSessions()
+	{
+		CComPtr<IMMDeviceEnumerator> deviceEnumerator;
+		CComPtr<IMMDevice> defaultDevice;
+		CComPtr<IAudioSessionManager2> audioSessionManager;
+		CComPtr<IAudioSessionEnumerator> audioSessionEnumerator;
+		CComPtr<IAudioSessionControl> audioSessionControl;
+		CComPtr<IAudioSessionControl2> audioSessionControl2;
+
+		auto hResult = CoInitializeEx(NULL, COINIT_APARTMENTTHREADED);
+		CheckHResultIsCorrect(hResult, "COM initialization error.");
+		_logger->LogInforamtion("COM initialization success.",
+			std::string(NAMEOF(AudioSessionFactory::GetAudioSessions)));
+
+		hResult = deviceEnumerator.CoCreateInstance(__uuidof(MMDeviceEnumerator), NULL, CLSCTX_INPROC_SERVER);
+		CheckHResultIsCorrect(hResult, "Create COM instanse error.");
+		_logger->LogInforamtion("Create device enumerator success.",
+			std::string(NAMEOF(AudioSessionFactory::GetAudioSessions)));
+
+		hResult = deviceEnumerator->GetDefaultAudioEndpoint(eRender, eMultimedia, &defaultDevice.p);
+		CheckHResultIsCorrect(hResult, "Get default audio endpoint error.");
+		_logger->LogInforamtion("Get default audio endpoint success",
+			std::string(NAMEOF(AudioSessionFactory::GetAudioSessions)));
+
+		hResult = defaultDevice->Activate(__uuidof(IAudioSessionManager2), CLSCTX_INPROC_SERVER, NULL, (void**)&audioSessionManager.p);
+		CheckHResultIsCorrect(hResult, "Activate audio session manager error.");
+		_logger->LogInforamtion("Activate audio session manager success.",
+			std::string(NAMEOF(AudioSessionFactory::GetAudioSessions)));
+
+		hResult = audioSessionManager->GetSessionEnumerator(&audioSessionEnumerator.p);
+		CheckHResultIsCorrect(hResult, "Get audio session enumerator error.");
+		_logger->LogInforamtion("Get audio session enumerator success.",
+			std::string(NAMEOF(AudioSessionFactory::GetAudioSessions)));
+
+		size_t sessionCount = 0;
+		hResult = audioSessionEnumerator->GetCount((int*)&sessionCount);
+		CheckHResultIsCorrect(hResult, "Get count audio session error.");
+		_logger->LogInforamtion("Get count audio session success.",
+			std::string(NAMEOF(AudioSessionFactory::GetAudioSessions)));
+
+		auto audioSessions = 
+			std::vector<std::shared_ptr<AudioSession>>();
+
+		for (size_t i = 0; i < sessionCount; i++)
+		{
+			hResult = audioSessionEnumerator->GetSession(i, &audioSessionControl.p);
+			CheckHResultIsCorrect(hResult, "Get audio session error.");
+			_logger->LogInforamtion("Get audio session success.",
+				std::string(NAMEOF(AudioSessionFactory::GetAudioSessions)));
+
+			audioSessionControl->QueryInterface<IAudioSessionControl2>(&audioSessionControl2.p);
+			DWORD pid;
+			hResult = audioSessionControl2->GetProcessId(&pid);
+			CheckHResultIsCorrect(hResult, "Get process id error.");
+			_logger->LogInforamtion("Get process id success",
+				std::string(NAMEOF(AudioSessionFactory::GetAudioSessions)));
+
+			auto processName = ProcessHandler::GetProcessNameByPid(pid);
+			CComPtr<ISimpleAudioVolume> volume;
+			audioSessionControl->QueryInterface<ISimpleAudioVolume>(&volume.p);
+
+			audioSessions.push_back(std::make_shared<AudioSession>(processName, pid, volume));
+
+			_logger->LogInforamtion("Create AudioSession success.",
+				std::string(NAMEOF(AudioSessionFactory::GetAudioSessions)));
+		}
+
+		CoUninitialize();
+
+		return audioSessions;
+	}
+}
--- AudioMix/src/SliderMapper.cpp
+++ AudioMix/src/SliderMapper.cpp
@@ -2,21 +2,19 @@
 
 namespace AudioMix
 {
-	SliderMapper::SliderMapper(std::shared_ptr<ILogger> logger)
+	SliderMapper::SliderMapper(std::shared_ptr<ILogger> logger, MapperConfig config)
 	{
 		if (logger == nullptr)
 		{
 			throw std::invalid_argument("Logger is null.");
 		}
 
+		_config = config;
 		_logger = logger;
-		InitMap();
-		_logger->LogInforamtion(
-			"SliderMapper was created successfully.", 
-			std::string(NAMEOF(SliderMapper::SliderMapper)));
+		_map = std::unordered_map<size_t, std::shared_ptr<AudioSession>>();
 	}
 
-	void SliderMapper::SetVolume(uint8_t sliderNumber, float volume)
+	void SliderMapper::SetVolume(size_t sliderNumber, float volume)
 	{
 		_map[sliderNumber]->SetVolume(volume);
 		_logger->LogInforamtion(
@@ -24,7 +22,7 @@ namespace AudioMix
 			std::string(NAMEOF(SliderMapper::SetVolume)));
 	}
 
-	float SliderMapper::GetVolume(uint8_t sliderNumber)
+	float SliderMapper::GetVolume(size_t sliderNumber)
 	{
 		_logger->LogInforamtion(
 			std::format("Get volume from {} slider", sliderNumber),
@@ -33,79 +31,13 @@ namespace AudioMix
 		return _map[sliderNumber]->GetVolume();
 	}
 
-	void SliderMapper::CheckHResult(HRESULT hResult, const std::string& errorMessage)
+	void SliderMapper::BindSlider(size_t sliderNumber, std::shared_ptr<AudioSession> audioSession)
 	{
-		if (FAILED(hResult))
+		if (sliderNumber + 1 > _config.sliderCount)
 		{
-			_logger->LogError(errorMessage, 
-				std::string(NAMEOF(SliderMapper::CheckHResult)));
-			throw std::runtime_error(errorMessage);
-		}
-	}
-
-	void SliderMapper::InitMap()
-	{
-		CComPtr<IMMDeviceEnumerator> deviceEnumerator;
-		CComPtr<IMMDevice> defaultDevice;
-		CComPtr<IAudioSessionManager2> audioSessionManager;
-		CComPtr<IAudioSessionEnumerator> audioSessionEnumerator;
-		CComPtr<IAudioSessionControl> audioSessionControl;
-		CComPtr<IAudioSessionControl2> audioSessionControl2;
-		
-		auto hResult = CoInitializeEx(NULL, COINIT_APARTMENTTHREADED);
-		CheckHResult(hResult, "COM initialization error.");
-		_logger->LogInforamtion("COM initialization success.",
-			std::string(NAMEOF(SliderMapper::InitMap)));
-
-		hResult = deviceEnumerator.CoCreateInstance(__uuidof(MMDeviceEnumerator), NULL, CLSCTX_INPROC_SERVER);
-		CheckHResult(hResult, "Create COM instanse error.");
-		_logger->LogInforamtion("Create device enumerator success.",
-			std::string(NAMEOF(SliderMapper::InitMap)));
-
-		hResult = deviceEnumerator->GetDefaultAudioEndpoint(eRender, eMultimedia, &defaultDevice.p);
-		CheckHResult(hResult, "Get default audio endpoint error.");
-		_logger->LogInforamtion("Get default audio endpoint success",
-			std::string(NAMEOF(SliderMapper::InitMap)));
-
-		hResult = defaultDevice->Activate(__uuidof(IAudioSessionManager2), CLSCTX_INPROC_SERVER, NULL, (void**)&audioSessionManager.p);
-		CheckHResult(hResult, "Activate audio session manager error.");
-		_logger->LogInforamtion("Activate audio session manager success.",
-			std::string(NAMEOF(SliderMapper::InitMap)));
-
-		hResult = audioSessionManager->GetSessionEnumerator(&audioSessionEnumerator.p);
-		CheckHResult(hResult, "Get audio session enumerator error.");
-		_logger->LogInforamtion("Get audio session enumerator success.",
-			std::string(NAMEOF(SliderMapper::InitMap)));
-
-		uint32_t sessionCount = 0;
-		hResult = audioSessionEnumerator->GetCount((int*)&sessionCount);
-		CheckHResult(hResult, "Get count audio session error.");
-		_logger->LogInforamtion("Get count audio session success.",
-			std::string(NAMEOF(SliderMapper::InitMap)));
-
-		for (int32_t i = 0; i < sessionCount; i++)
-		{
-			hResult = audioSessionEnumerator->GetSession(i, &audioSessionControl.p);
-			CheckHResult(hResult, "Get audio session error.");
-			_logger->LogInforamtion("Get audio session success.",
-				std::string(NAMEOF(SliderMapper::InitMap)));
-
-			audioSessionControl->QueryInterface<IAudioSessionControl2>(&audioSessionControl2.p);
-			DWORD pid;
-			hResult = audioSessionControl2->GetProcessId(&pid);
-			CheckHResult(hResult, "Get process id error.");
-			_logger->LogInforamtion("Get process id success",
-				std::string(NAMEOF(SliderMapper::InitMap)));
-
-			auto processName = ProcessHandler::GetProcessNameByPid(pid);
-			CComPtr<ISimpleAudioVolume> volume;
-			audioSessionControl->QueryInterface<ISimpleAudioVolume>(&volume.p);
-
-			_map[i] = std::make_unique<AudioSession>(processName, pid, volume);
-			_logger->LogInforamtion("Create AudioSession success.",
-				std::string(NAMEOF(SliderMapper::InitMap)));
+			throw std::invalid_argument("Slider number out of range.");
 		}
 
-		CoUninitialize();
+		_map[sliderNumber] = audioSession;
 	}
 }
